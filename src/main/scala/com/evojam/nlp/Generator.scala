6e20e5b3a169fa5731b0e646929b27b5ee3a9dcc
albert <albert@evojam.com>
2015-07-15 08:38:20 +0200

    Trim data

--- src/main/scala/com/evojam/nlp/Generator.scala
+++ src/main/scala/com/evojam/nlp/Generator.scala
@@ -63,7 +63,7 @@ object Generator extends App {
 
   println(s"Artists: ${artists.size}, Venues: ${venues.size}, DateTemplates: ${dateTemplates.size}, Expressions: ${expressions.size}")
 
-  val trainingSetSize = 500
+  val trainingSetSize = 10000
 
   val out = new FileOutputStream("out.train")
 
